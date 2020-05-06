7137acc5574643e6bda0ffb179c5d9aab7a98028
dobleklik <info>
2020-05-06 21:37:16 +0200

    añadir funcion setup

--- functions.php
+++ functions.php
@@ -7,3 +7,12 @@
  * @since Newsroom 1.0
  * 
  */
+
+ function newsroom_setup(){
+  //en esta función definimos que funciones vamos a utilizar del core de WP, a que elementos damos sopote
+
+      add-theme_support( 'post-thumbnails'); //recuperar la imagen destacada
+
+ }
+ //para que se produzca la acción y enganche añadimos lo siguiente: 
+ add_action('after_setup_theme', 'newsroom_setup');
