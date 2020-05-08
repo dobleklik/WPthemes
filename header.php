dab8ab143b534ec73cd883cdd511d54c5363546b
dobleklik <info>
2020-05-08 19:01:27 +0200

    añadiendo funciones en header

--- header.php
+++ header.php
@@ -9,9 +9,19 @@
 ?>
 
 <!DOCTYPE html>
-<html>
+<html <?php language_attributes(); ?>> <!--añadimos a la etiqueta el idioma a través de función WP-->
 <head>
+<meta charset="<?php bloginfo( 'charset' ); ?>"> 
+<!--Con la función bloginfo() obtenemos información de nuestro propio sitio, de la propia instalación
+del usuario-->
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<!--preparado para responsive-->
+
 <?php wp_head(); ?>
 </head>
 
-<body>
+<body <?php body_class():?>>
+<!--muy importante la función body_class() nos da información como la página donde nos encontramos,
+si el usuario está logado, etc-->
+    <?php wp_body_open(); ?> <!--añadimos esta función, dentro de ella podremos meter todas las
+    acciones que queramos que actuen justo tras la apertura de body-->
